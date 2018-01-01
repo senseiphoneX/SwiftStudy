5762a991d6231044c3c32ed8f18b37b340453f7e
senseiphoneX <senseiphonex@gmail.com>
2018-01-01 23:50:37 +0900

    chapter1 p.005

---
+++ chapter0101.playground/Contents.swift
@@ -0,0 +1,11 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+// Fig1-1
+var age: Int = 18
+var age2 = 18
+age2 = 20
+
+var width = 10.5, height = 9.25, kind = "Rect"
+var pack: Int = 0, x, y:Int, ram, rem: Double
