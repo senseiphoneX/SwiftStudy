7bd3226c3854697f370a39e099a6c3dda4d0c968
senseiphoneX <senseiphonex@gmail.com>
2018-01-02 23:50:02 +0900

    chapter1 p.006

--- chapter0101.playground/Contents.swift
+++ chapter0101.playground/Contents.swift
@@ -2,10 +2,26 @@
 
 import UIKit
 
-// Fig1-1
+// Fig1-1 p.5
 var age: Int = 18
 var age2 = 18
 age2 = 20
 
 var width = 10.5, height = 9.25, kind = "Rect"
 var pack: Int = 0, x, y:Int, ram, rem: Double
+
+// Fig1-2 p.6
+let name: String = "C. Alcott"
+let age3 = 2014 - 1997
+
+let meName = "E. Mably"; var myAge = 17
+
+let i: Int = 10
+var a: Double = Double(i)
+
+let r: Double = 0.1
+var f: Float = Float(r)
+
+let w: Double = 0.5
+let i2: Int = 10
+let b = w * Double(i2)
